dac84b4c5167537175ddd662884f04e18ce4d27a
MayaCoro <mayascoro@gmail.com>
2018-06-08 22:17:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # dog.rb
+class Dog 
+  def bark=(dog_bark)
+    @dog_bark = dog_bark
+  end
+  def bark
+    @dog_bark
+  end
+  def sit=(dog_sit)
+    @dog_sit = dog_sit
+  end
+  def sit
+    @dog_sit
+  end
+end
+
+woof = Dog.new
+woof.bark = "Woof!"
+puts "#{woof.bark}"
+
+sitting = Dog.new
+sitting.sit = "The dog is sitting"
+puts "#{sitting.sit}."
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
 # person.rb
+class Person
+  def talk=(person_talk)
+    @person_talk = person_talk
+  end
+  def talk
+    @person_talk
+  end
+  def walk=(person_walk)
+    @person_walk = person_walk
+  end
+  def walk
+    @person_walk
+  end
+end
+
+hello_world = Person.new
+hello_world.talk = "Hello world!"
+puts "#{hello_world.talk}"
+
+walking = Person.new
+walking.walk = "The person is walking"
+puts "#{walking.walk}."
